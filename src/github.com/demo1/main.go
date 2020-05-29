7042b0678eabba782c624ffbed7f4e748db6a4c0
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2020-05-29 22:38:46 +0100

    Added src directory

---
+++ src/github.com/demo1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Go-JumpStart!")
+	// c := add(5, 6)
+	// fmt.Println(c)
+	// z := boolean()
+	// fmt.Println(z)
+	// showvar()
+	// loop1()
+	// loop2()
+	// loop3()
+	// loop4()
+	// loop5()
+	// oddNumbers()
+	// evenNumbers()
+	days()
+}
