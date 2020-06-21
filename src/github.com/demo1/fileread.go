90b160df81a06314cb863f1553d2bc8b87aebb9f
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2020-06-21 22:35:15 +0100

    Read data and break at 100

--- src/github.com/demo1/fileread.go
+++ src/github.com/demo1/fileread.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 func readfiledemo() {
-	read1()
+	// read()
+	// read1()
+	read2()
 }
 
 func read() {
@@ -22,4 +25,18 @@ func read() {
 func read1() {
 	dat, _ := ioutil.ReadFile("src/github.com/demo1/dunkirk.txt")
 	fmt.Println(string(dat))
+}
+
+func read2() {
+	f, _ := os.Open("src/github.com/demo1/dunkirk.txt")
+	b := make([]byte, 100)
+
+	for {
+		r, _ := f.Read(b)
+		if r == 0 {
+			break
+		}
+		fmt.Println(r)
+		fmt.Println(string(b))
+	}
 }
