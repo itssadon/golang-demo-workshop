988d7e3dc51b67fe0ecfcf65a0476ab1d576334a
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2020-06-22 22:36:18 +0100

    Implemented interface

---
+++ src/github.com/demo1/interface.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type userprofile interface {
+	getname() string
+	getaddress() string
+	getcontact() map[string]string
+}
+
+func getuserinfo(u userprofile) {
+	fmt.Println("Name :", u.getname())
+	fmt.Println("Address :", u.getaddress())
+	fmt.Println("Contact Details :", u.getcontact())
+	fmt.Println("Email Extraction :", u.getcontact()["email"])
+}
+
+func interfacedemo() {
+	s := profile{
+		name:       "Sherlock",
+		address:    "221B Baker Street",
+		email:      "Sherlock@holmes.com",
+		contactnum: "18811904",
+	}
+
+	getuserinfo(s)
+}
